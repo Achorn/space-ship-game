c00f8135dd7decb63f5088b4b3c7d38d68e635a1
achorn <joshuaachorn@gmail.com>
2024-05-29 17:23:04 -0700

    adding utils to game class. added super() to timer class to trigger class extention

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,12 +1,19 @@
+import Debug from "./Utils/Debug";
+import Sizes from "./Utils/Sizes";
+
+let instance = null;
+
 export default class Game {
   constructor(canvas) {
-    console.log("game class constructor ");
+    if (instance) return instance;
+    instance = this;
 
     // Options
     this.canvas = canvas;
 
     // Setup
     this.debug = new Debug();
+    this.sizes = new Sizes();
   }
   resize() {}
   update() {}
--- src/Game/Utils/Sizes.js
+++ src/Game/Utils/Sizes.js
@@ -1,16 +1,18 @@
-import EventEmitter from "./EventEmitter";
+import EventEmitter from "./EventEmiter";
 
 export default class Sizes extends EventEmitter {
   constructor() {
+    super();
     // Setup
     this.width = window.innerWidth;
     this.height = window.innerHeight;
-    this.pixelRatio = Math.min(window.devicePixelRatio, 2); // sets limit on phones with high pixel ratios
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
     window.addEventListener("resize", (e) => {
       this.width = window.innerWidth;
       this.height = window.innerHeight;
-      this.pixelRatio = Math.min(window.devicePixelRatio, 2); // sets limit on phones with high pixel ratios
+      this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
       this.trigger("resize");
     });
   }
