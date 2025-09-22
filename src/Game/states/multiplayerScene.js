6bfe61d6f67953a62b41e123b95e2397141c9fc0
achorn <joshuaachorn@gmail.com>
2025-09-21 21:40:10 -0700

    https

--- src/Game/states/multiplayerScene.js
+++ src/Game/states/multiplayerScene.js
@@ -9,7 +9,7 @@ import DialogState from "./dialog/DialogState";
 import Game from "../Game";
 import EndCredits from "./EndCredits";
 import { io } from "socket.io-client";
-const URL = `http://${window.location.hostname}:3000`;
+const URL = `https://${window.location.hostname}:3000`;
 console.log(URL);
 import * as THREE from "three";
 
